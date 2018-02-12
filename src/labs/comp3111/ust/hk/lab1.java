8a6d440056a8d5af3381e6b5dafe4529ce1f1a3a
陈其勖 <@windows10.microdone.cn>
2018-02-12 15:33:11 +0800

    Add a congratulation message

--- src/labs/comp3111/ust/hk/lab1.java
+++ src/labs/comp3111/ust/hk/lab1.java
@@ -2,7 +2,9 @@ package labs.comp3111.ust.hk;
 
 public class lab1 {
 	public static void main(String[] args){
-		System.out.println("Lab1: Introduction to git and Github");
+		System.out.println("Lab1: Introduction to Git and Github");
+		System.out.println("Congratulations! You've learned Github basics");
+
 	}
 
 }
